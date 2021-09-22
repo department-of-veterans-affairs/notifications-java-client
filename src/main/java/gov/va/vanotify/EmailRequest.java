acbf4ca79ace1316397269754fce96790e0eb6fb
Filip Fafara <ffafara@thoughtworks.com>
2021-09-22 13:21:54 -0400

    Convert NotificationRequest interface to an abstract class to limit duplication of code. (#1)
    * Convert interface to an abstract class to limit duplication.

--- src/main/java/gov/va/vanotify/EmailRequest.java
+++ src/main/java/gov/va/vanotify/EmailRequest.java
@@ -8,71 +8,31 @@ import java.util.Map;
  * Represents email notification request
  * To create an instance use the static builder with fluent API
  * {@link EmailRequest.Builder}
+ * {@link NotificationRequest.Builder}
  */
-public class EmailRequest implements NotificationRequest {
-    private final String templateId;
-    private final String emailAddress;
-    private final Map<String, ?> personalisation;
-    private final String reference;
+public class EmailRequest extends NotificationRequest {
     private final String emailReplyToId;
-    private final String billingCode;
 
     private EmailRequest(Builder builder) {
-        this.templateId = builder.templateId;
-        this.emailAddress = builder.emailAddress;
-        this.personalisation = builder.personalisation;
-        this.reference = builder.reference;
+        super(builder);
         this.emailReplyToId = builder.emailReplyToId;
-        this.billingCode = builder.billingCode;
-
-        if (this.templateId == null || this.templateId.isEmpty()) throw new IllegalStateException("Missing templateId");
-        if (this.emailAddress == null || this.emailAddress.isEmpty()) throw new IllegalStateException("Missing emailAddress");
-    }
-
-    public String getTemplateId() {
-        return templateId;
+        if (this.recipient == null || this.recipient.isEmpty()) throw new IllegalStateException("Missing emailAddress");
     }
 
     public String getEmailAddress() {
-        return emailAddress;
-    }
-
-    public Map<String, ?> getPersonalisation() {
-        return personalisation;
-    }
-
-    public String getReference() {
-        return reference;
+        return this.recipient;
     }
 
     public String getEmailReplyToId() {
         return emailReplyToId;
     }
 
-    public String getBillingCode() {
-        return billingCode;
-    }
-
     @Override
     public JSONObject asJson() {
-        JSONObject body = new JSONObject();
+        JSONObject body = super.asJson();
 
-        body.put("template_id", templateId);
-
-        if(emailAddress != null && !emailAddress.isEmpty()) {
-            body.put("email_address", emailAddress);
-        }
-
-        if (personalisation != null && !personalisation.isEmpty()) {
-            body.put("personalisation", personalisation);
-        }
-
-        if(reference != null && !reference.isEmpty()){
-            body.put("reference", reference);
-        }
-
-        if(billingCode != null && !billingCode.isEmpty()){
-            body.put("billing_code", billingCode);
+        if(recipient != null && !recipient.isEmpty()) {
+            body.put("email_address", recipient);
         }
 
         if(emailReplyToId != null && !emailReplyToId.isEmpty())
@@ -85,23 +45,13 @@ public class EmailRequest implements NotificationRequest {
 
     /**
      * Fluent API Builder for EmailRequest
+     * Please see {@link NotificationRequest.Builder} for builder methods shared between all notification requests
      */
-    public static class Builder {
-        private String templateId;
-        private String emailAddress;
-        private Map<String, ?> personalisation;
-        private String reference;
+    public static class Builder extends NotificationRequest.Builder<EmailRequest, Builder> {
         private String emailReplyToId;
-        private String billingCode;
 
-
-        /**
-         * Sets <b>required</b> templateId.
-         * @param templateId    The template id is visible on the template page in the application.
-         * @return reference to itself (builder)
-         */
-        public Builder withTemplateId(String templateId) {
-            this.templateId = templateId;
+        @Override
+        protected Builder getInstance() {
             return this;
         }
 
@@ -111,28 +61,7 @@ public class EmailRequest implements NotificationRequest {
          * @return reference to itself (builder)
          */
         public Builder withEmailAddress(String emailAddress) {
-            this.emailAddress = emailAddress;
-            return this;
-        }
-
-        /**
-         * Sets <b>optional</b> personalisations.
-         * @param personalisation   Map representing the placeholders for the template if any. For example, key=name value=Bob
-         * @return reference to itself (builder)
-         */
-        public Builder withPersonalisation(Map<String, ?> personalisation) {
-            this.personalisation = personalisation;
-            return this;
-        }
-
-        /**
-         * Sets <b>optional</b> reference
-         * @param reference A reference specified by the service for the notification. Get all notifications can be filtered by this reference.
-         *                  This reference can be unique or used used to refer to a batch of notifications.
-         * @return reference to itself (builder)
-         */
-        public Builder withReference(String reference) {
-            this.reference = reference;
+            this.withRecipient(emailAddress);
             return this;
         }
 
@@ -149,21 +78,11 @@ public class EmailRequest implements NotificationRequest {
         }
 
         /**
-         *
-         * @param billingCode   A billing code specified by the service for the notification.
-         *                      Used to group notifications for billing and reporting.
-         * @return reference to itself (builder)
-         */
-        public Builder withBillingCode(String billingCode) {
-            this.billingCode = billingCode;
-            return this;
-        }
-
-        /**
          * Builds {@link EmailRequest}
          * @return <code>EmailRequest</code>
          * @throws IllegalStateException if any required fields are missing
          */
+        @Override
         public EmailRequest build() {
             return new EmailRequest(this);
         }
--- src/main/java/gov/va/vanotify/NotificationRequest.java
+++ src/main/java/gov/va/vanotify/NotificationRequest.java
@@ -2,6 +2,143 @@ package gov.va.vanotify;
 
 import org.json.JSONObject;
 
-public interface NotificationRequest {
-    JSONObject asJson();
+import java.util.Map;
+
+public abstract class NotificationRequest {
+    protected final String templateId;
+    protected final String recipient;
+    protected final Map<String, ?> personalisation;
+    protected final String reference;
+    protected final String billingCode;
+
+    protected NotificationRequest(Builder builder) {
+        this.templateId = builder.templateId;
+        this.recipient = builder.recipient;
+        this.personalisation = builder.personalisation;
+        this.reference = builder.reference;
+        this.billingCode = builder.billingCode;
+
+        if (this.templateId == null || this.templateId.isEmpty()) throw new IllegalStateException("Missing templateId");
+    }
+
+    public String getTemplateId() {
+        return templateId;
+    }
+
+    public Map<String, ?> getPersonalisation() {
+        return personalisation;
+    }
+
+    public String getReference() {
+        return reference;
+    }
+
+    public String getBillingCode() {
+        return billingCode;
+    }
+
+    protected JSONObject asJson() {
+        JSONObject body = new JSONObject();
+
+        body.put("template_id", templateId);
+
+        if (personalisation != null && !personalisation.isEmpty()) {
+            body.put("personalisation", personalisation);
+        }
+
+        if(reference != null && !reference.isEmpty()){
+            body.put("reference", reference);
+        }
+
+        if(billingCode != null && !billingCode.isEmpty()){
+            body.put("billing_code", billingCode);
+        }
+
+        return body;
+    }
+
+    public abstract static class Builder<T extends NotificationRequest, B extends Builder> {
+        protected String templateId;
+        protected String recipient;
+        protected Map<String, ?> personalisation;
+        protected String reference;
+        protected String emailReplyToId;
+        protected String billingCode;
+
+        protected abstract B getInstance();
+
+        /**
+         * Sets <b>required</b> recipient.
+         * @param recipient  The email address or phone number
+         * @return reference to itself (builder)
+         */
+        protected B withRecipient(String recipient) {
+            this.recipient = recipient;
+            return this.getInstance();
+        }
+
+        /**
+         * Sets <b>required</b> templateId.
+         * @param templateId    The template id is visible on the template page in the application.
+         * @return reference to itself (builder)
+         */
+        public B withTemplateId(String templateId) {
+            this.templateId = templateId;
+            return this.getInstance();
+        }
+
+        /**
+         * Sets <b>optional</b> personalisations.
+         * @param personalisation   Map representing the placeholders for the template if any. For example, key=name value=Bob
+         * @return reference to itself (builder)
+         */
+        public B withPersonalisation(Map<String, ?> personalisation) {
+            this.personalisation = personalisation;
+            return this.getInstance();
+        }
+
+        /**
+         * Sets <b>optional</b> reference
+         * @param reference A reference specified by the service for the notification. Get all notifications can be filtered by this reference.
+         *                  This reference can be unique or used used to refer to a batch of notifications.
+         * @return reference to itself (builder)
+         */
+        public B withReference(String reference) {
+            this.reference = reference;
+            return this.getInstance();
+        }
+
+        /**
+         * Sets <b>optional</b> emailReplyToId.
+         * Will use the default service email reply to address if not provided.
+         * @param emailReplyToId    An optional identifier for a reply to email address for the notification, rather than use the service default.
+         *                          Service emailReplyToIds can be accessed via the service settings / manage email reply to addresses page.
+         * @return reference to itself (builder)
+         */
+        public B withEmailReplyToId(String emailReplyToId) {
+            this.emailReplyToId = emailReplyToId;
+            return this.getInstance();
+        }
+
+        /**
+         *
+         * @param billingCode   A billing code specified by the service for the notification.
+         *                      Used to group notifications for billing and reporting.
+         * @return reference to itself (builder)
+         */
+        public B withBillingCode(String billingCode) {
+            this.billingCode = billingCode;
+            return this.getInstance();
+        }
+
+        /**
+         * Abstract method for building a NotificationRequest
+         * Use one of concrete implementations:
+         * {@link SmsRequest.Builder}
+         * {@link EmailRequest.Builder}
+         *
+         * @throws IllegalStateException if any required fields are missing
+         */
+        public abstract T build();
+    }
 }
